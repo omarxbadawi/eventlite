f5347e67e33d493e9cfdaaa22c81e836e81d11e9
Omar Badawi <omar.badawi-2@student.manchester.ac.uk>
2022-04-29 12:04:22 +0200

    Add test for VenuesControllerApi

---
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerApiTest.java
@@ -0,0 +1,72 @@
+package uk.ac.man.cs.eventlite.controllers;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.endsWith;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Collections;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import uk.ac.man.cs.eventlite.assemblers.VenueModelAssembler;
+import uk.ac.man.cs.eventlite.config.Security;
+import uk.ac.man.cs.eventlite.dao.VenueService;
+import uk.ac.man.cs.eventlite.entities.Event;
+import uk.ac.man.cs.eventlite.entities.Venue;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(VenuesControllerApi.class)
+@Import({ Security.class, VenueModelAssembler.class })
+public class VenuesControllerApiTest {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@MockBean
+	private VenueService venueService;
+
+	@Test
+	public void getIndexWhenNoVenues() throws Exception {
+		when(venueService.findAll()).thenReturn(Collections.<Venue>emptyList());
+
+		mvc.perform(get("/api/venues").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(handler().methodName("getVenues")).andExpect(jsonPath("$.length()", equalTo(2)))
+				.andExpect(jsonPath("$._links.self.href", endsWith("/api/venues")));
+
+		verify(venueService).findAll();
+	}
+
+	@Test
+	public void getIndexWithVenues() throws Exception {
+		Venue v = new Venue();
+		v.setId(0);
+		v.setName("Venue");
+		v.setCapacity(500);
+		v.setRoad("Road");
+		v.setPostcode("M00 000");
+		when(venueService.findAll()).thenReturn(Collections.<Venue>singletonList(v));
+
+		mvc.perform(get("/api/venues").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(handler().methodName("getVenues")).andExpect(jsonPath("$.length()", equalTo(2)))
+				.andExpect(jsonPath("$._links.self.href", endsWith("/api/venues")))
+				.andExpect(jsonPath("$._embedded.venues.length()", equalTo(1)));
+
+		verify(venueService).findAll();
+	}
+}
