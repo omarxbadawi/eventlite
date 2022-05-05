e4fcf4c19c309b3faf489426958e7452756fcb1d
Joshua Coates <joshua.coates@student.manchester.ac.uk>
2022-05-05 17:01:31 +0100

    Update initial data

--- src/main/java/uk/ac/man/cs/eventlite/config/data/InitialDataLoader.java
+++ src/main/java/uk/ac/man/cs/eventlite/config/data/InitialDataLoader.java
@@ -37,24 +37,26 @@ public class InitialDataLoader {
 			}
 			// Build and save initial venues here.
 			Venue venue = new Venue();
-			venue.setName("Venue 1");
-			venue.setRoad("23 Manchester Road");
-			venue.setPostcode("E14 3BD");
-			venue.setCapacity(100);
+			venue.setName("Mediacity");
+			venue.setRoad("DPL Studio, University of Salford\n" +
+					"\n" +
+					"Mediacity Campus");
+			venue.setPostcode("M50 2HE");
+			venue.setCapacity(300);
 			venue.setLongLat();
 			venueService.save(venue);
 			Venue venue2 = new Venue();
-			venue2.setName("Venue 2");
-			venue2.setRoad("Highland Road");
-			venue2.setPostcode("S43 2EZ");
-			venue2.setCapacity(200);
+			venue2.setName("Binary Bar and Grill");
+			venue2.setRoad("5 Arundel Street");
+			venue2.setPostcode("M15 4JZ");
+			venue2.setCapacity(50);
 			venue2.setLongLat();
 			venueService.save(venue2);
 			Venue venue3 = new Venue();
-			venue3.setName("Venue 3");
-			venue3.setRoad("19 Acacia Avenue");
-			venue3.setPostcode("WA15 8QY");
-			venue3.setCapacity(300);
+			venue3.setName("Fairways Lodge");
+			venue3.setRoad("George Street, Prestwich");
+			venue3.setPostcode("M25 9WS");
+			venue3.setCapacity(150);
 			venue3.setLongLat();
 			venueService.save(venue3);
 
@@ -64,39 +66,39 @@ public class InitialDataLoader {
 			}
 			// Build and save initial events here.
 			Event event1 = new Event();
-			event1.setName("Event 1");
-			event1.setDescription("This is the best event ever");
+			event1.setName("TEDxUniversityofSalford");
+			event1.setDescription("In the spirit of ideas worth spreading, TED has created a program called TEDx. TEDx is a program of local, self-organized events that bring people together to share a TED-like experience. Our event is a University event called TEDxUniversityofSalford.");
 			event1.setVenue(venue);
-			event1.setTime(LocalTime.now().minusHours(2));
-			event1.setDate(LocalDate.now().minusDays(1));
+			event1.setTime(LocalTime.of(18,0));
+			event1.setDate(LocalDate.of(2022, 6, 18));
 			eventService.save(event1);
 			Event event2 = new Event();
-			event2.setName("Event 2");
-			event2.setDescription("This is the second best event ever");
+			event2.setName("Castlefield Comedy Club");
+			event2.setDescription("Here at Castlefield Comedy Club, we host some of the UK's best comedy talent, award-winning comics and future stars. Previous comedians have included (Live at The Apollo's ) Jonny Pelham, Brenna Reece, Tez Ilyas and Jack Carroll to name a few.");
 			event2.setVenue(venue2);
-			event2.setTime(LocalTime.now().plusHours(1));
-			event2.setDate(LocalDate.now().plusDays(1));
+			event2.setTime(LocalTime.of(19,30));
+			event2.setDate(LocalDate.of(2022, 6, 5));
 			eventService.save(event2);
 			Event event3 = new Event();
-			event3.setName("Event 3");
-			event3.setDescription("This is the third best event ever");
+			event3.setName("Fashion & Beauty Show");
+			event3.setDescription("We’re bringing together the best local fashion and beauty businesses for an afternoon of demonstrations, talks, treatments, tips & tricks of the trade plus loads of local independent brands selling everything from jewellery and more!");
 			event3.setVenue(venue3);
-			event3.setTime(LocalTime.now().plusHours(2));
-			event3.setDate(LocalDate.now().plusDays(2));
+			event3.setTime(LocalTime.of(12,0));
+			event3.setDate(LocalDate.of(2022, 6, 12));
 			eventService.save(event3);
 			Event event4 = new Event();
-			event4.setName("Event 4");
-			event4.setDescription("This is the third best event ever");
-			event4.setVenue(venue3);
-			event4.setTime(LocalTime.now().minusHours(5));
-			event4.setDate(LocalDate.now().minusDays(1));
+			event4.setName("Imola Grand Prix Screening");
+			event4.setDescription("Join us in Italy themed fancy dress for the Imola GP. Prizes available for best costumes.");
+			event4.setVenue(venue2);
+			event4.setTime(LocalTime.of(14,0));
+			event4.setDate(LocalDate.of(2022, 4, 24));
 			eventService.save(event4);
 			Event event5 = new Event();
-			event5.setName("Event 5");
-			event5.setDescription("This is the third best event ever");
-			event5.setVenue(venue2);
-			event5.setTime(LocalTime.now().plusHours(1));
-			event5.setDate(LocalDate.now().plusDays(2));
+			event5.setName("Makers Markets");
+			event5.setDescription("The Makers Markets return to MediaCity. Discover the very finest local food, drink, art, design, vintage and modern craft carefully selected from Manchester artisans and businesses local to the North West.");
+			event5.setVenue(venue);
+			event5.setTime(LocalTime.of(10,0));
+			event5.setDate(LocalDate.of(2022, 4, 15));
 			eventService.save(event5);
 		};
 	}
